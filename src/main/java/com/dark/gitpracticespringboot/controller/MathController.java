aaae4bc35af877d5a31183148443d99560edb45f
hariharan_zincat <hariharan.vasudevan@dialog.lk>
2022-01-12 19:18:24 +0530

    name lower function

--- src/main/java/com/dark/gitpracticespringboot/controller/MathController.java
+++ src/main/java/com/dark/gitpracticespringboot/controller/MathController.java
@@ -1,6 +1,9 @@
 package com.dark.gitpracticespringboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,12 +11,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/math")
 public class MathController {
 
+    @Autowired
+    Environment env;
+
     /**
      * http://localhost:9090/math/test
      * */
     @GetMapping("test")
     public String test(){
         return "test";
+    }
+
+    /**
+     * http://localhost:9090/math/name/lower/KamAl
+     * */
+    @GetMapping("name/lower/{name}")
+    public String nameLower(@PathVariable String name){
+
+        int length = name.length();
+
+        int suffix = length + 4;
+        name = "_" + name.toLowerCase() + "_";
+        String prefix = env.getProperty("name.lower.value");
+
+        String response = suffix + name + prefix;
+
+        return response;
     }
 
 }
