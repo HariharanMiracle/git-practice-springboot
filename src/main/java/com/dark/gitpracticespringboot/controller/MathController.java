ac230f91f1ef2aeb3a2f74f1677b877b91a443ea
hariharan_zincat <hariharan.vasudevan@dialog.lk>
2022-01-12 19:14:24 +0530

    name upper function

--- src/main/java/com/dark/gitpracticespringboot/controller/MathController.java
+++ src/main/java/com/dark/gitpracticespringboot/controller/MathController.java
@@ -1,12 +1,20 @@
 package com.dark.gitpracticespringboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/math")
 public class MathController {
+
+    @Autowired
+    Environment env;
 
     /**
      * http://localhost:9090/math/test
@@ -14,6 +22,23 @@ public class MathController {
     @GetMapping("test")
     public String test(){
         return "test";
+    }
+
+    /**
+     * http://localhost:9090/math/name/upper/KamAl
+     * */
+    @GetMapping("name/upper/{name}")
+    public String nameUpper(@PathVariable String name){
+
+        int length = name.length();
+
+        int suffix = length + 4;
+        name = "_" + name.toUpperCase() + "_";
+        String prefix = env.getProperty("name.upper.value");
+
+        String response = suffix + name + prefix;
+
+        return response;
     }
 
 }
